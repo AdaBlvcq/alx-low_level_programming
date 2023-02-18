e6707b7398870eb6b99aabee171040934545de89
Nana Afia <nnafya@gmail.com>
2023-02-17 18:32:32 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
+
 /**
- * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line, using putchar
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
 
-	for (n = 48; n < 58; n++)
+	char ch = 'a';
+
+	while (ch <= 'z')
 	{
-		putchar(n);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
 	putchar('\n');
 	return (0);
