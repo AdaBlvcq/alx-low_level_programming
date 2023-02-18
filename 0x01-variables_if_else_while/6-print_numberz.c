b76fe1aa9aaba26cff581af864ce52a2368344cb
Nana Afia <nnafya@gmail.com>
2023-02-17 18:36:00 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,6 +13,6 @@ int main(void)
 	{
 		putchar(n);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
