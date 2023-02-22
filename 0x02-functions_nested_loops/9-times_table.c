f018ab1453b63e10d49ad95cef9da3b1ab3e92c7
Nana Afia <nnafya@gmail.com>
2023-02-22 10:49:31 -0800

    10-add.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,9 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i =0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j < 19; j++)
+		for (j = 0; j < 19; j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -23,14 +23,14 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_Putchar(k + '0');
-			}else if (k >= 10)
+			} else if (k >= 10)
 			{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-	        	}
-			_putchar('\n');
 		}
+			_putchar('\n');
+	}
 }
