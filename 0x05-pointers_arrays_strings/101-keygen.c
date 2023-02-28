b0ea666628904435794d6b574920fa2a25307131
Nana Afia <nnafya@gmail.com>
2023-02-27 21:02:39 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - program that generates random valid
  * passwords for the program 101-crackme
@@ -12,7 +14,9 @@ int main(void)
 	int i, sum, n;
 
 	sum = 0;
+
 	srand(time(NULL));
+
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
@@ -21,10 +25,11 @@ int main(void)
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
-		i	sum += n;
+			sum += n;
 			putchar(n + '0');
 			break;
 		}
 	}
+
 	return (0);
 }
