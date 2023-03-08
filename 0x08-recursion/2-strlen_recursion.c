489f89042d0465a1c8133cedba4bf9de5a9e3ce1
Nana Afia <nnafya@gmail.com>
2023-03-08 10:40:30 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen_recursion - Return the lenght of a string
+ * _strlen_recursion - Returns the lenght of a string
  * @s: The string to be measured
  *
  * Return: The lenght of the string
@@ -14,4 +14,5 @@ int _strlen_recursion(char *s)
 		len++;
 		len += _strlen_recursion(s + 1);
 	}
+	return (len);
 }
