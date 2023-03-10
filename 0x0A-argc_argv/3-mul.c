1bc2a3988ede8951b44756695638683e49c7cf58
Nana Afia <nnafya@gmail.com>
2023-03-10 14:49:32 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - multiply 2 int
+ * main - prints the multiplication of two integers
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 if true, 1 if false
