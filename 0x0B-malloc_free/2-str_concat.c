cd3561df39e7630dca7e4ef33d4be16d851ddb88
Nana Afia <nnafya@gmail.com>
2023-03-14 12:58:30 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,4 @@
-#include "holberton"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -10,40 +10,33 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, k = 0, len2 = 0;
 
-	while (s1 && s1[len1])
-		len++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	s3 = malloc(sizeof(char) * (i + j + 1));
 
 	if (s3 == NULL)
-
-	return (NULL);
-
-	i = 0;
-	j = 0;
-
-	if (s1)
 	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
+
+		free(s3);
+		return (NULL);
 	}
 
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[i];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
+	for (k = 0; k < i; k++)
+			s3[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= len2; k++, j++)
+		s3[k] = s2[j];
 	return (s3);
 }
