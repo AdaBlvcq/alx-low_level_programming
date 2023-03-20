6c6de47c43c9c845f50c07f21fdcd865371b30c6
Nana Afia <nnafya@gmail.com>
2023-03-20 09:32:53 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
-	{
-		d->name = nam;
-		d->age = age;
-		d->owner = owner;
-	}
+		d = malloc(sizeof(struct dog));
+	d->name = nam;
+	d->age = age;
+	d->owner = owner;
 }
