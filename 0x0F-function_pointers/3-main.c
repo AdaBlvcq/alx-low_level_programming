caf4db2a9fe9fe220efa3f19e8f4570bd4ffbb5b
Nana Afia <nnafya@gmail.com>
2023-03-22 15:29:21 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "3.calc.h"
+#include "3-calc.h"
 /**
- * main - prints the result of main function only
- * @argc: number of argunebts supplied
- * @argv: array of pointers to the arguments
- * Return: Always 0
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
-int main(int_attribute_((_unused_)) argc, char *argv[])
+int main(int _attribute((__unused_)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -21,12 +22,13 @@ int main(int_attribute_((_unused_)) argc, char *argv[])
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
-	if (get_op_func(op) == NULL ||
-			op{1} != '\0')
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
-		primtf("Error\n");
+		printf("Error\n");
 		exit(99);
 	}
+
 	if ((*op == '/' && num2 == 0) ||
 		(*op == '%' && num2 == 0))
 	{
