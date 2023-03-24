bed5058280a201a964349898a7b0eafd8a32744d
Nana Afia <nnafya@gmail.com>
2023-03-24 13:51:21 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
-#include "variadic_functions.h"
+#include "variadic_functions.h"i
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
  * print_all - prints anything
  * @format: a list of types of arguments passed to the function
