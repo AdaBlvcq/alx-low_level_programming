0642a3491b064da37db736d9d600a41540d2362a
Nana Afia <nnafya@gmail.com>
2023-04-09 14:26:16 -0700

    1-create_file.c done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+/*
+ * create_file - funtion that creates a file
+ * @filename - is the name of the file to create
+ * @text_content - is a NULL terminated string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int myfile;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	myfile = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (myfile == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+	bytes_written = write(myfile, text_content, strlen(text_content));
+
+	if (bytes_written == -1)
+	{
+		close(myfile);
+		return (-1);
+	}
+	}
+	close(myfile);
+	return (1);
+}
