921d676c016cb169187b91897f45344a06ee50f3
Nana Afia <nnafya@gmail.com>
2023-04-27 14:21:47 -0700

    1-print_binary.c file added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * print_binary - Prints the binary representation of a number
- * @n: The number to print in binary
- *
- * Return: None
+ * _power - calculate (base and power)
+ * @base: base of the exponet
+ * @pow: power of the exponet
+ * Return: value of base and power
+ */
+unsigned long int _power(unsigned int base, unsigned int pow)
+{
+	unsigned long int num;
+	unsigned int counter;
+
+	num = 1;
+	for (counter = 1; counter <= pow; counter++)
+		num *= base;
+	return (num);
+}
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: num of prented
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-	unsigned int counter;
+	unsigned long int dev, result;
+	char flag;
 
-	for (counter = 0; counter < (sizeof(unsigned long int) * 8); counter++)
+	flag = 0;
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (dev != 0)
 	{
-		if ((n & (mask << counter)) == (mask << counter))
-			putchar('1');
-		else
-			putchar('0');
+		result = n & dev;
+		if (result == dev)
+		{
+			flag = 1;
+			_putchar('1');
+
+		}
+		else if (flag == 1 || dev == 1)
+		{
+			_putchar('0');
+		}
+		dev >>= 1;
 	}
 }
