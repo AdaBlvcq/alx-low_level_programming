ae152d73e1de4f49aae46eca7ef5bf693e17a7c9
Nana Afia <nnafya@gmail.com>
2023-05-23 07:27:40 -0700

    100-operations.c

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,67 +1,35 @@
 #include <stdio.h>
-
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
-int mod(int a, int b);
-
 /**
-* add - adds two integers
-* @a: first integer to add
-* @b: second integer to add
-* Return: the sum
+* add - add two integers
+* sub - subtract two integers
+* mul - multiply two integers
+* div - divide two integers
+* mod - module of two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: 0 Success
 */
-
-int add(int a, int b)
+int add(int i, int k)
 {
-	return (a + b);
+	return (i + k);
 }
 
-/**
-* sub - subtracts two integers
-* @a: first integer to add
-* @b: second integer to add
-* Return: the subtract
-*/
-
-int sub(int a, int b)
+int sub(int i, int k)
 {
-	return (a - b);
+	return (i - k);
 }
 
-/**
-* mul - multiplies two integers
-* @a: first integer to add
-* @b: second integer to add
-* Return: the multiple
-*/
-
-int mul(int a, int b)
+int mul(int i, int k)
 {
-	return (a * b);
+	return (i * k);
 }
 
-/**
-* div - divideds two integers
-* @a: first integer to add
-* @b: second integer to add
-* Return: the dividend integer
-*/
-
-int div(int a, int b)
+int div(int i, int k)
 {
-	return (a / b);
+	return (i / k);
 }
 
-/**
-  mod - finds the modulus of two integers
-  @a: first integer to add
-  @b: second integer to add
-  Return: the modulus
-*/
-
-int mod(int a, int b)
+int mod(int i, int k)
 {
-	return (a % b);
+	return (i % k);
 }
